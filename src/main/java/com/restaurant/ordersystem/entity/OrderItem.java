aacd2e37c8b15a085504bb94d6e69942f90866c3
David Carrasco <davidack123456789@gmail.com>
2026-03-22 23:40:56 -0500

    Apartado de entity finalizado

--- src/main/java/com/restaurant/ordersystem/entity/OrderItem.java
+++ src/main/java/com/restaurant/ordersystem/entity/OrderItem.java
@@ -1,5 +1,57 @@
 package com.restaurant.ordersystem.entity;
+import jakarta.persistence.*;
 
+
+@Entity
+@Table(name = "order_items")
 public class OrderItem {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+@ManyToOne
+@JoinColumn(name = "order_id")
+private Order order;
+@ManyToOne
+@JoinColumn(name = "product_id")
+private Product product;
+private Integer quantity;
+private Double unitPrice;
+private Double subtotal;
+public  OrderItem(){
 
+}
+//GETTERS Y SETTER
+   public Long getId(){
+    return id;
+   }
+   public Order getOrder(){
+    return  order;
+   }
+   public  void  setOrder(Order order){
+    this.order = order;
+   }
+   public  Product getProduct(){
+    return product;
+   }
+   public  void setProduct(Product product){
+    this.product = product;
+   }
+   public  Integer getQuantity(){
+    return quantity;
+   }
+   public void setQuantity(Integer quantity){
+    this.quantity = quantity;
+   }
+   public Double getUnitPrice(){
+    return unitPrice;
+   }
+   public void setUnitPrice(Double unitPrice){
+    this.unitPrice = unitPrice;
+   }
+   public Double getSubtotal(){
+    return subtotal;
+   }
+   public  void setSubtotal(Double subtotal){
+    this.subtotal = subtotal;
+   }
 }
---
+++ src/main/java/com/restaurant/ordersystem/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.restaurant.ordersystem.repository;
+
+public class UserRepository {
+}
