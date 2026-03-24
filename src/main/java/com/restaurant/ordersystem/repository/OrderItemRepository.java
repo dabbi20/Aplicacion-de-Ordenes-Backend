aa3a386d154324f442abaa7c9ef9511dedc5b126
David Carrasco <davidack123456789@gmail.com>
2026-03-23 22:01:16 -0500

    En la parte del service creo mi producservice para los diferentes estados de mis productos

--- src/main/java/com/restaurant/ordersystem/repository/OrderItemRepository.java
+++ src/main/java/com/restaurant/ordersystem/repository/OrderItemRepository.java
@@ -1,5 +1,6 @@
-package com.restaurant.ordersystem.repository;
-import com.restaurant.ordersystem.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+package com.restaurant.ordersystem.repository;//Indica que la interface esta en el paquete repository
+import com.restaurant.ordersystem.entity.OrderItem;//Importamos la entidad OrderItem con la que trabajaremos
+import org.springframework.data.jpa.repository.JpaRepository;//Importamos JPARepository este trae los metodos listos para crear el CRUD
+public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {//Quiero un repositorio que maneje la entidad User y cuyo ID es de tipo Long
+
 }
---
+++ src/main/java/com/restaurant/ordersystem/service/ProductService.java
@@ -0,0 +1,39 @@
+package com.restaurant.ordersystem.service;
+import com.restaurant.ordersystem.entity.Product;
+import com.restaurant.ordersystem.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import  java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+public Product saveProduct(Product product){
+        return productRepository.save(product);
+}
+public List <Product> getAllProducts(){
+        return productRepository.findAll();
+}
+public Optional<Product> getProductById(Long id){
+        return productRepository.findById(id);
+}
+public Product updateProduct(Long id, Product updatedProduct){
+    Product existingProduct = productRepository.findById(id).orElseThrow(()-> new RuntimeException("Producto no encontrado"));
+
+    existingProduct.setName(updatedProduct.getName());
+    existingProduct.setDescription(updatedProduct.getDescription());
+    existingProduct.setPrice(updatedProduct.getPrice());
+    existingProduct.setAvailable(updatedProduct.getAvailable());
+
+return productRepository.save(existingProduct);
+}
+
+    public void deleteProduct(Long id){
+        productRepository.deleteById(id);
+    }
+}
