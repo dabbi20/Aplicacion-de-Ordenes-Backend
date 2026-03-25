1fd2e64349ca12738783e56c024025fc9ba1214d
David Carrasco <davidack123456789@gmail.com>
2026-03-24 20:08:30 -0500

    CRUD realizado completamente

---
+++ src/main/java/com/restaurant/ordersystem/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.restaurant.ordersystem.controller;
+import com.restaurant.ordersystem.entity.Product;
+import com.restaurant.ordersystem.repository.ProductRepository;
+import com.restaurant.ordersystem.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import  java.util.List;
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final ProductService productService;
+
+
+    public ProductController(ProductService productService){
+this.productService = productService;
+    }
+
+    //CREAR PRODUCTO
+    @PostMapping
+    public  Product createProduct(@RequestBody Product product){
+        return productService.saveProduct(product);
+    }
+    //OBTENER POR ID
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id){
+        return productService.getProductById(id).orElseThrow(()-> new RuntimeException("Producto no encontrado"));
+    }
+    //ACTUALIZAR
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product){
+        return productService.updateProduct(id, product);
+    }
+
+    //BORRAR
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
+    }
+
+}
