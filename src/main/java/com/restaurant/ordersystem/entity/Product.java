f1dc14ccceaf1e0f34adc1fb6fe555991088bdd0
David Carrasco <davidack123456789@gmail.com>
2026-04-06 23:24:40 -0500

    actualizacion en product para respectiva conexion

--- src/main/java/com/restaurant/ordersystem/entity/Product.java
+++ src/main/java/com/restaurant/ordersystem/entity/Product.java
@@ -1,49 +1,69 @@
 package com.restaurant.ordersystem.entity;
+
 import jakarta.persistence.*;
 
-@Entity// Indica que esta clase es una entidad, es decir, una tabla en la base de datos
-@Table(name = "products")// Define el nombre de la tabla en la BD
-public class Product {// Clase pública que representa la tabla products
-    @Id// Define la clave primaria (ID)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)// Indica que el ID se genera automáticamente en la base de datos (autoincremental en PostgreSQL)
+@Entity
+@Table(name = "products")
+public class Product {
 
-    private  Long id;// Atributo privado, solo se accede mediante getters y setters, representa el identificador
-    private  String name;// Atributo privado de tipo String que representa el nombre del producto
-    private  String description;// Atributo privado de tipo String que representa la descripcion del producto
-    private  Double price;// Atributo privado de tipo Double que representa el precio del producto
-    private  Boolean available;// Atributo privado de tipo Boolean que representa el available del producto
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public  Product(){
-    }// Constructor vacío requerido por JPA para poder crear objetos automáticamente
+    private String name;
+    private String description;
+    private Double price;
+    private Boolean available;
+    private String imageUrl;
 
-    // getters y setters (luego podemos usar Lombok)
-    public Long getId() {// Método público para obtener el ID
+    public Product() {
+    }
+
+    public Long getId() {
         return id;
     }
-    public String getName(){
-        return name;// Método público para obtener el nombre
-    }
-    public void  setName(String name){
-        this.name = name;// Método público para modificar el nombre
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public  String getDescription(){
-        return  description;
+    public String getName() {
+        return name;
     }
-    public  void setDescription(String description){
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 
-    public  Double getPrice(){
+    public Double getPrice() {
         return price;
     }
-    public  void  setPrice(Double price){
+
+    public void setPrice(Double price) {
         this.price = price;
     }
-    public Boolean getAvailable(){
+
+    public Boolean getAvailable() {
         return available;
     }
-    public  void  setAvailable(Boolean available){
+
+    public void setAvailable(Boolean available) {
         this.available = available;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
--- src/main/java/com/restaurant/ordersystem/service/ProductService.java
+++ src/main/java/com/restaurant/ordersystem/service/ProductService.java
@@ -1,37 +1,45 @@
 package com.restaurant.ordersystem.service;
+
 import com.restaurant.ordersystem.entity.Product;
 import com.restaurant.ordersystem.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
-import  java.util.List;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ProductService {
+
     private final ProductRepository productRepository;
+
     public ProductService(ProductRepository productRepository){
         this.productRepository = productRepository;
     }
 
-public Product saveProduct(Product product){
+    public Product saveProduct(Product product){
         return productRepository.save(product);
-}
-public List <Product> getAllProducts(){
+    }
+
+    public List<Product> getAllProducts(){
         return productRepository.findAll();
-}
-public Optional<Product> getProductById(Long id){
+    }
+
+    public Optional<Product> getProductById(Long id){
         return productRepository.findById(id);
-}
-public Product updateProduct(Long id, Product updatedProduct){
-    Product existingProduct = productRepository.findById(id).orElseThrow(()-> new RuntimeException("Producto no encontrado"));
+    }
 
-    existingProduct.setName(updatedProduct.getName());
-    existingProduct.setDescription(updatedProduct.getDescription());
-    existingProduct.setPrice(updatedProduct.getPrice());
-    existingProduct.setAvailable(updatedProduct.getAvailable());
+    public Product updateProduct(Long id, Product updatedProduct){
+        Product existingProduct = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
 
-return productRepository.save(existingProduct);
-}
+        existingProduct.setName(updatedProduct.getName());
+        existingProduct.setDescription(updatedProduct.getDescription());
+        existingProduct.setPrice(updatedProduct.getPrice());
+        existingProduct.setAvailable(updatedProduct.getAvailable());
+        existingProduct.setImageUrl(updatedProduct.getImageUrl());
+
+        return productRepository.save(existingProduct);
+    }
 
     public void deleteProduct(Long id){
         productRepository.deleteById(id);
